6ed68cecdcf1dfed6972c5194581f321c70d199f
ashwanthdurairaj <69207745+ashwanthdurairaj@users.noreply.github.com>
2023-05-16 19:39:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,42 @@
-const path = require('path');
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv');
-dotenv.config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+// const path = require('path');
+// const express = require('express');
+// const colors = require('colors');
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const { errorHandler } = require('./middleware/errorMiddleware');
+// const connectDB = require('./config/db');
+// const port = process.env.PORT || 5000;
 
-connectDB();
+// connectDB();
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/users', require('./routes/userRoutes'));
-app.use('/managers', require('./routes/managerRoutes'));
-app.use('/admin', require('./routes/adminRoutes'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// });
+// app.use('/api/goals', require('./routes/goalRoutes'));
+// app.use('/users', require('./routes/userRoutes'));
+// app.use('/managers', require('./routes/managerRoutes'));
+// app.use('/admin', require('./routes/adminRoutes'));
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+// // Serve frontend
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//     )
+//   );
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// app.listen(port, () => console.log(`Server started on port ${port}`));
